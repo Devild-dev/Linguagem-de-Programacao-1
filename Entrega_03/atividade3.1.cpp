840fd21d7271a082360e1e84c9fe87ce7dff7de7
Davi Barbosa <devildbar@gmail.com>
2022-10-16 21:04:57 -0300

    /update
    Atualização do programa, foram criadas funções e realizadas correções.

--- Entrega_03/atividade3.1.cpp
+++ Entrega_03/atividade3.1.cpp
@@ -6,36 +6,38 @@
 struct tipoAluno{
 	int ID;
 	char respostas[TAM_Q];
-	int nota=0;
+	int nota;
 };
 
+float porcent(float a, int t);
+int  frequencia(tipoAluno *a, int t);
+
 int main(){
-	int inicio = 9997;
+	int inicio = 9995;
 	int fim = 9999;
-	float total = fim-inicio;
+	int total = fim-inicio;
 	float aprovados = 0;
 	char gabarito[TAM_Q] = {'a','a','a','a','a','a','a','a','a','a'};
-	tipoAluno alunos[2];
-	int frequencia[TAM_Q+1];
+	tipoAluno alunos[total];
+	float porcentagem;
+	int maior = 0;
+	int id = inicio+1;
+	int nota = 0;
 	
-	int aux = inicio+1;
 	for(int i=0;i<total;i++){
-		alunos[i].ID = aux;
-		
+		alunos[i].ID = id;
+		nota = 0;
 		for(int j=0;j<TAM_Q;j++){
 			printf("Digite a %da resposta do %do aluno:\n", j+1, i+1);
 			scanf("%c", &alunos[i].respostas[j]);
-			setbuf(stdin,NULL);			
+			setbuf(stdin,NULL);		
 			if(alunos[i].respostas[j] == gabarito[j]){	
-				alunos[i].nota++;
-			}		
+				nota++;
+				alunos[i].nota = nota;
+			}					
 		}	
 		system("cls");
-	    aux++;	
-	}
-	
-	for(int i=0;i<total;i++){
-		printf("A nota do aluno de ID: %d eh %d \n", alunos[i].ID , alunos[i].nota);
+	    id++;	
 	}
 	
 	for(int i=0;i<total;i++){
@@ -43,30 +45,47 @@ int main(){
 			aprovados++;
 		}
 	}
+
+	porcentagem = porcent(aprovados, total);
+	maior = frequencia(alunos, total);
 	
-	float porcentagem = (aprovados/total)*100;
+	for(int i=0;i<total;i++)
+		printf("A nota do aluno de ID: %d eh %d \n", alunos[i].ID , alunos[i].nota);
+		
 	printf("A quantidade de alunos aprovados: %.0f\n", aprovados);
 	printf("Porcentagem de aprovados: %.0f%%\n", porcentagem);
-	printf("O total de alunos eh: %.0f\n", total);
+	printf("O total de alunos eh: %.0d\n", total);
 	
-	for(int i=0;i<11;i++){//preenche a vetor com zeros
-		frequencia[i] = 0;
-	}
 	
-	for(int i=0;i<total;i++){
-		for(int j=0;j<=TAM_Q;j++){		
-			if(alunos[i].nota == j){
-				frequencia[j]++;
-			}		
-		}		
-	}
 	
-	for(int j=0;j<=TAM_Q;j++){
-		printf("Frequencia da nota %d eh %d\n",j, frequencia[j]);
-	}
+	printf("A nota de maior frequencia eh: %d", maior);
 	getch();
 }
 
-/*for(int i=0;i<Q;i++){
-		printf("%c\n", gabarito[i]);	
-	}*/
+float porcent(float a, int t){
+	float p;
+	p = (a/t)*100;
+	return p;
+}
+
+int  frequencia(tipoAluno *a, int t){
+	int maior = 0;
+	int f[TAM_Q+1];	
+	for(int i=0;i<=TAM_Q;i++){//preenche a vetor com zeros
+		f[i] = 0;
+	}
+	for(int i=0;i<t;i++){
+		for(int j=0;j<=TAM_Q;j++){					
+			if(a[i].nota == j){	
+				f[j]++;
+			}		
+		}
+	}
+	for(int i=0;i<=TAM_Q;i++){
+		if(f[i] > maior){
+			maior = i;	
+		}
+	}
+	return maior;	
+}
+
