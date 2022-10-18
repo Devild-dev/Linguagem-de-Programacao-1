d80cb9b41dc4e6f8f78e783cff9fbb8bdab1a1d2
Davi Barbosa <devildbar@gmail.com>
2022-10-17 23:55:15 -0300

    \fix
    Consertando alguns bugs na função frequencia.

--- Entrega_03/atividade3.1.cpp
+++ Entrega_03/atividade3.1.cpp
@@ -13,14 +13,14 @@ float porcent(float a, int t);
 int  frequencia(tipoAluno *a, int t);
 
 int main(){
-	int inicio = 9995;
+	int inicio = 9996;
 	int fim = 9999;
 	int total = fim-inicio;
 	float aprovados = 0;
 	char gabarito[TAM_Q] = {'a','a','a','a','a','a','a','a','a','a'};
 	tipoAluno alunos[total];
+	int maiorF = 0;
 	float porcentagem;
-	int maior = 0;
 	int id = inicio+1;
 	int nota = 0;
 	
@@ -29,7 +29,7 @@ int main(){
 		nota = 0;
 		for(int j=0;j<TAM_Q;j++){
 			printf("Digite a %da resposta do %do aluno:\n", j+1, i+1);
-			scanf("%c", &alunos[i].respostas[j]);
+			scanf(" %c", &alunos[i].respostas[j]);
 			setbuf(stdin,NULL);		
 			if(alunos[i].respostas[j] == gabarito[j]){	
 				nota++;
@@ -47,7 +47,7 @@ int main(){
 	}
 
 	porcentagem = porcent(aprovados, total);
-	maior = frequencia(alunos, total);
+	maiorF = frequencia(alunos, total);
 	
 	for(int i=0;i<total;i++)
 		printf("A nota do aluno de ID: %d eh %d \n", alunos[i].ID , alunos[i].nota);
@@ -55,7 +55,7 @@ int main(){
 	printf("A quantidade de alunos aprovados: %.0f\n", aprovados);
 	printf("Porcentagem de aprovados: %.0f%%\n", porcentagem);
 	printf("O total de alunos eh: %.0d\n", total);
-	printf("A nota de maior frequencia eh: %d", maior);
+	printf("A nota de maior frequencia eh: %d", maiorF);
 	getch();
 }
 
@@ -66,21 +66,26 @@ float porcent(float a, int t){
 }
 
 int  frequencia(tipoAluno *a, int t){
+	int f[TAM_Q+1];
 	int maior = 0;
-	int f[TAM_Q+1];	
+	int aux = 0;
+	
 	for(int i=0;i<=TAM_Q;i++){//preenche a vetor com zeros
 		f[i] = 0;
 	}
-	for(int i=0;i<t;i++){
-		for(int j=0;j<=TAM_Q;j++){					
-			if(a[i].nota == j){	
+	for(int i=0;i<t;i++){	
+		for(int j=0;j<=TAM_Q;j++){									
+			if(a[i].nota == j){
 				f[j]++;
-			}		
-		}
+			}
+					
+		}	
 	}
+	
 	for(int i=0;i<=TAM_Q;i++){
-		if(f[i] > maior){
-			maior = i;	
+		if(f[i] > aux){
+			maior = i;
+			aux = f[i];	
 		}
 	}
 	return maior;	
